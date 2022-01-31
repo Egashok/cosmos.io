5b7d7179ec136eb4b103e8b019286249997b76cc
egashok <55044715+Egashok@users.noreply.github.com>
2022-01-31 21:47:19 +0600

    da

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function(){
     $('.slider__box').slick({
-        prevArrow:'<img class="slider__arrow left" src="../images/Arl.svg">',
-        nextArrow:'<img class="slider__arrow right" src="../images/Arr.svg">',
+        prevArrow:'<img class="slider__arrow left" src="images/Arl.svg">',
+        nextArrow:'<img class="slider__arrow right" src="images/Arr.svg">',
     });
     $('.menu-btn').on('click',function(){
         $('.menu__list').toggleClass('active');
