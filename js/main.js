7b5d7373b7eaa269ab9db269d89fc042f0d9500e
egashok <55044715+Egashok@users.noreply.github.com>
2022-01-31 21:39:20 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.slider__box').slick({
+        prevArrow:'<img class="slider__arrow left" src="../images/Arl.svg">',
+        nextArrow:'<img class="slider__arrow right" src="../images/Arr.svg">',
+    });
+    $('.menu-btn').on('click',function(){
+        $('.menu__list').toggleClass('active');
+      
+    })
+});
